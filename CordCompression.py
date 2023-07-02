f52c066eb7066ec0cb41c4d054409bbcd6e3cb5b
BeefSityuu <yukiimai1030@gmail.com>
2023-07-02 15:41:03 +0900

    Update CordCompression.py

--- CordCompression.py
+++ CordCompression.py
@@ -2,5 +2,5 @@
 def cordCompress(lst):
     tempList = sorted(set(lst))
     re = { v: i for i, v in enumerate(tempList) }
-    return list(map(lambda v: re[v]+1, lst))
+    return list(map(lambda v: re[v], lst))
 
